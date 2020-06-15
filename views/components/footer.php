fd423f089ccc41f8cc3f2c002c813fc1708088c7
Rustam @SecondFry Gubaydullin <secondfry@gmail.com>
2020-06-15 23:26:03 +0300

    body closing tag on newline

--- views/components/footer.php
+++ views/components/footer.php
@@ -11,5 +11,6 @@
     </div>
   </div>
 <script src="/bundle/main.js"></script>
-<script src="https://kit.fontawesome.com/f44fb68376.js" crossorigin="anonymous"></script></body>
+<script src="https://kit.fontawesome.com/f44fb68376.js" crossorigin="anonymous"></script>
+</body>
 </html>
